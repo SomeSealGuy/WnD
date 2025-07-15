bcedeb52a163862a3c091a5f1f135fdaf5ffb454
SomeSealGuyTM <simon.pisarek@avenga.com>
2025-07-15 10:41:45 +0200

    serve all files

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var http = require("http");
 var fs = require("fs");
-var index = fs.readFileSync("index.html");
 
-http
-  .createServer((_req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(index);
-  })
-  .listen(8080);
+
+function serve(request, response) {
+    if (request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(fs.readFileSync("./server/Arf.html"));
+    } else {
+        if (request.url.endsWith(".css")) {
+            response.writeHead(200, { "Content-Type": "text/css" });
+        } else {
+            response.writeHead(200, { "Content-Type": "text/html" });
+        }
+        response.end(fs.readFileSync('./server/'+request.url));
+    }
+    
+}
+
+
+http.createServer(serve).listen(8080);
